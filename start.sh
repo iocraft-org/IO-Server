73ac73bc1744627974e95e7d7ca764434d64bce0
io-craft <io_craft@protonmail.com>
2019-03-23 10:50:15 +0000

    Automation, bleh!

--- start.sh
+++ start.sh
@@ -13,16 +13,16 @@ MCR_PASS=$(cat ~/.mcr_pass)
 MONGO=$(cat ~/.mongo)
 JAR="Thermos-1.7.10-1614-server.jar"
 RAM_MIN="1G"
-RAM_MAX="8G"
-CPU_CORES="3"
+RAM_MAX="3G"
+CPU_CORES="1"
 #CURRENT_VERSION="$(curl -s 'https://raw.githubusercontent.com/worldautomation/WA-Launcher-Pack/master/app/assets/distribution.json' | awk '/version/{i++}i==2{print; exit}' | awk -F "\"*:\"*" '{print $2}' | cut -c 3- | cut -c -7)"
-if ! screen -list | grep -q "iocraft"; then
+if ! screen -list | grep -q "iogame"; then
 	echo "Server is starting!"
 	bash push.sh
 	rm server.properties
 	sed s/MCR_PASS/$MCR_PASS/g server.properties.template > server.properties
 	#sudo screen -dmS resist sudo java -Xmx8G -Xms6G -jar forge-1.12.2-14.23.5.2768-universal.jar nogui
-	sudo screen -dmS iocraft sudo java -server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=$CPU_CORES -XX:+AggressiveOpts -Xms$RAM_MIN -Xmx$RAM_MAX -jar $JAR nogui
+	sudo screen -dmS iogame sudo java -server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=$CPU_CORES -XX:+AggressiveOpts -Xms$RAM_MIN -Xmx$RAM_MAX -jar $JAR nogui
 else
 	echo "Server already started!";
 fi
