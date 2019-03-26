2f8f2c05ba0a5cb5bd8f66952316c6811525c754
io-craft <io_craft@protonmail.com>
2019-03-26 08:35:53 +0000

    Automation, bleh!

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 cd "$(dirname "$0")"
 rm -rf logs/*.gz
 DISCORD=$(cat ~/.discord_token)
-WP_USER=$(cat ~/.wp_user)
-WP_PASS=$(cat ~/.wp_pass)
+WP_USER=$(cat ~/.wp_username)
+WP_PASS=$(cat ~/.wp_password)
 MYSQL_DATABASE=$(cat ~/.mysql_database)
-MYSQL_USER=$(cat ~/.mysql_user)
+MYSQL_USER=$(cat ~/.mysql_username)
 MYSQL_PASSWORD=$(cat ~/.mysql_password)
-MYSQL_HOST=$(cat ~/.mysql_host)
+MYSQL_HOST=$(cat ~/.mysql_hostname)
 IRC_PASSWORD=$(cat ~/.irc_password)
 MCR_PASS=$(cat ~/.mcr_pass)
 MONGO=$(cat ~/.mongo)
@@ -21,6 +21,21 @@ if ! screen -list | grep -q "iogame"; then
 	bash push.sh
 	rm server.properties
 	sed s/MCR_PASS/$MCR_PASS/g server.properties.template > server.properties
+	##
+	## Start Credentials
+	##
+
+	# MySQL OnTime
+	rm plugins/OnTime/config.yml;
+	sed "s#MYSQL_DATABASE#$MYSQL_DATABASE#g" plugins/OnTime/config.yml.template > plugins/OnTime/config.yml;
+	sed -i "s/MYSQL_USERNAME/$MYSQL_USERNAME/g" plugins/OnTime/config.yml;
+	sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" plugins/OnTime/config.yml;
+	sed -i "s/MYSQL_HOSTNAME/$MYSQL_HOSTNAME/g" plugins/OnTime/config.yml;
+
+	##
+	## End Credentials
+	##
+
 	#sudo screen -dmS resist sudo java -Xmx8G -Xms6G -jar forge-1.12.2-14.23.5.2768-universal.jar nogui
 	sudo screen -dmS iogame sudo java -server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=$CPU_CORES -XX:+AggressiveOpts -Xms$RAM_MIN -Xmx$RAM_MAX -jar $JAR nogui
 else
