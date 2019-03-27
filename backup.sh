2a3ed70dd774981e15a4156368a61e8ce388d86a
io-craft <io_craft@protonmail.com>
2019-03-27 03:33:20 +0000

    Automation, bleh!

--- backup.sh
+++ backup.sh
@@ -4,5 +4,5 @@ TIMESTAMP=$(TZ=":US/Central" date +%Y-%m-%d_%R:%S)
 FILENAME=$(echo $TIMESTAMP.zip)
 mkdir -p /storage/backups
 zip -r /storage/backups/$FILENAME world
-cp /storage/backups/$FILENAME /storage/server/latest-world-backup.zip
+cp /storage/backups/$FILENAME /storage/IO-Server/latest-world-backup.zip
 sh push.sh
